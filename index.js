daaebb6f652fd57d0ab55c77b7fc9aa9c40e72c1
asd741 <a22833593@gmail.com>
2018-12-02 20:56:45 +0800

    rwd適配

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const indexSass=require('./index.sass');
+function changeRem(){
+    let w=window.innerWidth;
+    document.documentElement.style.fontSize=(w>=1024?"1024px":w+"px");
+}
+changeRem();
+window.onresize=changeRem;
