ca07f7bcfe1375238f7947f3118227cf5a12ec05
asd741 <a22833593@gmail.com>
2018-12-05 21:47:31 +0800

    完成章節選擇的css部分

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const indexSass=require('./index.sass');
-// function changeRem(){
-//     let w=window.innerWidth;
-//     document.documentElement.style.fontSize=(w>=1024?"1024px":w+"px");
-// }
-// changeRem();
-// window.onresize=changeRem;
+window.onload=function(){
+    aChapterList=document.querySelectorAll(".page-footer .chapter-box .chapter-list");
+    for(let i=-1;aChapterList[++i];){
+        aChapterList[i].addEventListener('click',goChapterPage.bind("",i+1));
+    }
+    function goChapterPage(chapter){
+        document.getElementsByClassName('index-footer')[0].style.display="none";
+        document.getElementsByClassName('index-content')[0].style.display="none";
+        document.getElementsByClassName('chapter-content')[0].style.display="block";
+        document.getElementsByClassName('chapter-nav')[0].style.display="flex";
+    }
+}
