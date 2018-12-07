4bc573f9be2b8c7811ed0141d1bcc3ab469c01a4
asd741 <a22833593@gmail.com>
2018-12-07 17:31:19 +0800

    遇到漫畫無法翻頁的bug

--- index.js
+++ index.js
@@ -1,39 +1,63 @@
 const indexSass=require('./index.sass');
 window.onload=function(){
-    aChapterList=document.querySelectorAll(".page-footer .chapter-box .chapter-list");
-    oChapterNavBtn=document.querySelector(".chapter-nav .chapter");
-    oPageNavBtn=document.querySelector(".chapter-nav .page");
+    if(window.location.hash===''){
+        goIndex();
+    }else{
+        goChapter(1);
+    }
+    
+    // window.location.href.indexOf('#')
+    // goIndex();
+    aIndexChapterList=document.querySelectorAll(".page-footer .chapter-box .chapter-list");
+    oChapterBtn=document.querySelector(".chapter-nav .chapter");
+    oPageBtn=document.querySelector(".chapter-nav .page");
     aGoIndexBtn=document.getElementsByClassName('go-index-btn');
+    aChapterMenuList=document.querySelectorAll('.options .chapter .menu li');
+    aPageMenuList=document.querySelectorAll('.options .page .menu li');
     for(let i=-1;aGoIndexBtn[++i];){
-        aGoIndexBtn[i].addEventListener('click',goIndexPage);
+        aGoIndexBtn[i].addEventListener('click',goIndex);
     }
-    for(let i=-1;aChapterList[++i];){
-        aChapterList[i].addEventListener('click',goChapterPage.bind("",i+1));
+    for(let i=-1;aIndexChapterList[++i];){
+        aIndexChapterList[i].addEventListener('click',goChapter.bind("",i+1));
     }
-    oChapterNavBtn.addEventListener('click',function(){
+    for(let i=-1;aChapterMenuList[++i];){
+        aChapterMenuList[i].addEventListener('click',changeComicChapter.bind("",i+1));
+    }
+    for(let i=-1;aPageMenuList[++i];){
+        aPageMenuList[i].addEventListener('click',changeComicPage.bind("",i+1));
+    }
+    oChapterBtn.addEventListener('click',function(){
         this.getElementsByClassName('menu')[0].classList.toggle('none');
-        oPageNavBtn.getElementsByClassName('menu')[0].classList.add('none');
+        oPageBtn.getElementsByClassName('menu')[0].classList.add('none');
         window.event?event.stopPropagation():cancelBubble=true;
     },true);
-    oPageNavBtn.addEventListener('click',function(){
+    oPageBtn.addEventListener('click',function(){
         this.getElementsByClassName('menu')[0].classList.toggle('none');
-        oChapterNavBtn.getElementsByClassName('menu')[0].classList.add('none');
+        oChapterBtn.getElementsByClassName('menu')[0].classList.add('none');
         window.event?event.stopPropagation():cancelBubble=true;
     },true);
     document.documentElement.addEventListener('click',function(){
-        oChapterNavBtn.getElementsByClassName('menu')[0].classList.add('none');
-        oPageNavBtn.getElementsByClassName('menu')[0].classList.add('none');
+        oChapterBtn.getElementsByClassName('menu')[0].classList.add('none');
+        oPageBtn.getElementsByClassName('menu')[0].classList.add('none');
     })
-    function goChapterPage(i){
-        window.history.pushState('','',"chapter"+i);
+    function goChapter(i){
+        window.history.pushState('','chapter'+i,"#chapter"+i);
         document.getElementsByClassName('index-footer')[0].style.display="none";
         document.getElementsByClassName('index-content')[0].style.display="none";
-        document.getElementsByClassName('chapter-content')[0].style.display="block";
+        document.getElementsByClassName('chapter-content')[0].style.display="flex";
         document.getElementsByClassName('chapter-nav')[0].style.display="flex";
         document.getElementsByClassName('chapter-footer')[0].style.display="block";
     }
-    function goIndexPage(){
-        window.history.pushState('',"",'index.htm');
+    function changeComicPage(i){
+        alert("!!!");
+        window.history.pushState('','page'+i,"#chapter"+i+"/page"+i);
+    }
+    function changeComicChapter(i){
+        alert("???");
+        window.history.pushState('','chapter'+i,"#chapter"+i);
+    }
+    function goIndex(){
+        window.history.pushState('',"index",'index.htm');
         document.getElementsByClassName('index-footer')[0].style.display="block";
         document.getElementsByClassName('index-content')[0].style.display="block";
         document.getElementsByClassName('chapter-content')[0].style.display="none";
