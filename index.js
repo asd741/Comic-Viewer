816a2722182006f8a07a4f245bc54417444ca13d
asd741 <a22833593@gmail.com>
2018-12-07 22:44:45 +0800

    遇到頁數傳參問題

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const indexSass=require('./index.sass');
 window.onload=function(){
+    const state={
+        Chapter:1,
+        ChapterPage:1
+    };
     if(window.location.hash===''){
-        goIndex();
+        goHomePage();
     }else{
         goChapter(1);
     }
-    
     // window.location.href.indexOf('#')
-    // goIndex();
+    // goHomePage();
     aIndexChapterList=document.querySelectorAll(".page-footer .chapter-box .chapter-list");
     oChapterBtn=document.querySelector(".chapter-nav .chapter");
     oPageBtn=document.querySelector(".chapter-nav .page");
-    aGoIndexBtn=document.getElementsByClassName('go-index-btn');
+    agoHomePageBtn=document.getElementsByClassName('go-index-btn');
     aChapterMenuList=document.querySelectorAll('.options .chapter .menu li');
     aPageMenuList=document.querySelectorAll('.options .page .menu li');
-    for(let i=-1;aGoIndexBtn[++i];){
-        aGoIndexBtn[i].addEventListener('click',goIndex);
+    oComicImg=document.getElementById('comic-img');
+    oComicPrevBtn=document.querySelector(".chapter-content .prev");
+    oComicNextBtn=document.querySelector(".chapter-content .next");
+    oComicPrevBtn.addEventListener('click',changeComicPage.bind('',--state.ChapterPage));
+    oComicNextBtn.addEventListener('click',changeComicPage.bind('',++state.ChapterPage));
+    for(let i=-1;agoHomePageBtn[++i];){
+        agoHomePageBtn[i].addEventListener('click',goHomePage);
     }
     for(let i=-1;aIndexChapterList[++i];){
         aIndexChapterList[i].addEventListener('click',goChapter.bind("",i+1));
@@ -30,18 +38,18 @@ window.onload=function(){
         this.getElementsByClassName('menu')[0].classList.toggle('none');
         oPageBtn.getElementsByClassName('menu')[0].classList.add('none');
         window.event?event.stopPropagation():cancelBubble=true;
-    },true);
+    });
     oPageBtn.addEventListener('click',function(){
         this.getElementsByClassName('menu')[0].classList.toggle('none');
         oChapterBtn.getElementsByClassName('menu')[0].classList.add('none');
         window.event?event.stopPropagation():cancelBubble=true;
-    },true);
+    });
     document.documentElement.addEventListener('click',function(){
         oChapterBtn.getElementsByClassName('menu')[0].classList.add('none');
         oPageBtn.getElementsByClassName('menu')[0].classList.add('none');
     })
     function goChapter(i){
-        window.history.pushState('','chapter'+i,"#chapter"+i);
+        window.history.pushState(state,'chapter'+i,"#chapter"+i);
         document.getElementsByClassName('index-footer')[0].style.display="none";
         document.getElementsByClassName('index-content')[0].style.display="none";
         document.getElementsByClassName('chapter-content')[0].style.display="flex";
@@ -49,15 +57,18 @@ window.onload=function(){
         document.getElementsByClassName('chapter-footer')[0].style.display="block";
     }
     function changeComicPage(i){
-        alert("!!!");
-        window.history.pushState('','page'+i,"#chapter"+i+"/page"+i);
+        oComicImg.src='img/page'+i+'.png';
+        state.ChapterPage=i;
+        window.history.pushState(state,`chapter${state.Chapter}/page${state.ChapterPage}`,`#chapter${state.Chapter}/page${state.ChapterPage}`);
     }
     function changeComicChapter(i){
-        alert("???");
-        window.history.pushState('','chapter'+i,"#chapter"+i);
+        state.Chapter=i;
+        console.log(state.ChapterPage);
+
+        window.history.pushState(state,`chapter${state.Chapter}/page1`,`#chapter${state.Chapter}/page1`);
     }
-    function goIndex(){
-        window.history.pushState('',"index",'index.htm');
+    function goHomePage(){
+        window.history.pushState(state,"index",'index.htm');
         document.getElementsByClassName('index-footer')[0].style.display="block";
         document.getElementsByClassName('index-content')[0].style.display="block";
         document.getElementsByClassName('chapter-content')[0].style.display="none";
